659ed972912a170f3ed7d9042dbcfd35bb30dd8e
siyuanucd <ucdzhou@ucdavis.edu>
2013-10-10 16:07:33 -0700

    update HW0.py

--- HW0/HW0.py
+++ HW0/HW0.py
@@ -1,3 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This temporary script file is located here:
+C:\Users\siyuanzhou\.spyder2\.temp.py
+"""
+
 import re
 import os
 import sys
@@ -23,11 +31,11 @@ def printFizzBuzz(start,end):
 
 printFizzBuzz(1,100)
 
+
 # --------------------   problem 2  --------------------#
 '''Write a program that generates 10,000 uniform random numbers between 0 and
 (call this ), and 10,000 uniform random numbers between 0 and 1 (call this ).
 You will then have 10,000 pairs of random numbers.'''
-
 x = list()
 y = list()
 for i in range(10000):
@@ -35,6 +43,39 @@ for i in range(10000):
    y.append(random.uniform(0,1))
 u = [y[i]*math.cos(x[i]) for i in range(10000)]
 v = [y[i]*math.sin(x[i]) for i in range(10000)]
-line = plt.plot(u,v)
-matplotlib.scatter(u, v, c=z)
-plt.show()
+matplotlib.pyplot.scatter(u,v)
+matplotlib.pyplot.show()
+''' distribution is uniform on a circle'''
+
+# -----------------  problem 3 is written in R ----------#
+# -----------------  problem 4  -------------------------#
+
+def AR(n):
+    pho = 0.8
+    y = list()
+    y.append(0)
+    for i in range(1,n):
+        y.append(pho*y[i-1]+random.normalvariate(0,1))
+    return y
+y = AR(1000)
+plot(range(1000),y,'g')
+show()
+
+m = numpy.zeros((200,1000))
+for i in range(200):
+    m[i,]=AR(1000)
+m = m.T
+
+subplot(1,2,1)
+plot(range(200),m.mean(0),'g')
+subplot(1,2,2)
+plot(range(200),m.var(0),'g')
+show()
+
+
+subplot(1,2,1)
+plot(range(1000),m.mean(1),'g')
+subplot(1,2,2)
+plot(range(1000),m.var(1),'g')
+show()
+
